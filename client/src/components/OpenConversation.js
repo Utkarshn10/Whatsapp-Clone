4da54d08963be5da1d8d01c09e3d6fda0967eac4
Utkarsh10 <utkarshnagar2000@gmail.com>
2021-03-23 18:00:55 +0530

    Added

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,47 +1,79 @@
-import React, { useState, useCallback } from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap'
-import { useConversations } from '../contexts/ConversationsProvider';
-
+import React, {useState, useCallback} from "react";
+import {Form, InputGroup, Button} from "react-bootstrap";
+import {useConversations} from "../contexts/ConversationsProvider";
+import {bgImage} from "../images/Soothe_logo.jpeg";
+import {ListGroup} from "react-bootstrap";
+import {useContacts} from "../contexts/ContactsProvider";
+import Navbar from "react-bootstrap/Navbar";
 export default function OpenConversation() {
-  const [text, setText] = useState('')
-  const setRef = useCallback(node => {
+  const [text, setText] = useState("");
+  const setRef = useCallback((node) => {
     if (node) {
-      node.scrollIntoView({ smooth: true })
+      node.scrollIntoView({smooth: true});
     }
-  }, [])
-  const { sendMessage, selectedConversation } = useConversations()
+  }, []);
+  const {conversations, sendMessage, selectedConversation} = useConversations();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     sendMessage(
-      selectedConversation.recipients.map(r => r.id),
+      selectedConversation.recipients.map((r) => r.id),
       text
-    )
-    setText('')
+    );
+    setText("");
   }
+  // style={{
+  //   backgroundImage:
+  //     "url(" +
+  //     "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+  //     ")",
+  //   backgroundSize: "cover",
+  // }}
 
+  // {
+  /* <img src="bgImage"></img> */
+  // }
   return (
     <div className="d-flex flex-column flex-grow-1">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Soothe</Navbar.Brand>
+      </Navbar>
+
       <div className="flex-grow-1 overflow-auto">
         <div className="d-flex flex-column align-items-start justify-content-end px-3">
+          {/* <div
+            
+          > */}
           {selectedConversation.messages.map((message, index) => {
-            const lastMessage = selectedConversation.messages.length - 1 === index
+            const lastMessage =
+              selectedConversation.messages.length - 1 === index;
             return (
               <div
                 ref={lastMessage ? setRef : null}
                 key={index}
-                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end align-items-end' : 'align-items-start'}`}
+                className={`my-1 d-flex flex-column ${
+                  message.fromMe
+                    ? "align-self-end align-items-end"
+                    : "align-items-start"
+                }`}
               >
                 <div
-                  className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                  className={`rounded px-2 py-1 ${
+                    message.fromMe ? "bg-primary text-white" : "border"
+                  }`}
+                >
                   {message.text}
                 </div>
-                <div className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
-                  {message.fromMe ? 'You' : message.senderName}
+                <div
+                  className={`text-muted small ${
+                    message.fromMe ? "text-right" : ""
+                  }`}
+                >
+                  {message.fromMe ? "You" : message.senderName}
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </div>
@@ -52,15 +84,18 @@ export default function OpenConversation() {
               as="textarea"
               required
               value={text}
-              onChange={e => setText(e.target.value)}
-              style={{ height: '75px', resize: 'none' }}
+              onChange={(e) => setText(e.target.value)}
+              style={{height: "75px", resize: "none"}}
             />
             <InputGroup.Append>
-              <Button type="submit">Send</Button>
+              <Button style={{backgroundColor: "#"}} type="submit">
+                Send
+              </Button>
             </InputGroup.Append>
           </InputGroup>
         </Form.Group>
       </Form>
+      {/* </div> */}
     </div>
-  )
+  );
 }
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react'
-import { Tab, Nav, Button, Modal } from 'react-bootstrap'
-import Conversations from './Conversations'
-import Contacts from './Contacts'
-import NewContactModal from './NewContactModal'
-import NewConversationModal from './NewConversationModal'
+import React, {useState} from "react";
+import {Tab, Nav, Button, Modal} from "react-bootstrap";
+import Conversations from "./Conversations";
+import Contacts from "./Contacts";
+import NewContactModal from "./NewContactModal";
+import NewConversationModal from "./NewConversationModal";
 
-const CONVERSATIONS_KEY = 'conversations'
-const CONTACTS_KEY = 'contacts'
+const CONVERSATIONS_KEY = "conversations";
+const CONTACTS_KEY = "contacts";
 
-export default function Sidebar({ id }) {
-  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
-  const [modalOpen, setModalOpen] = useState(false)
-  const conversationsOpen = activeKey === CONVERSATIONS_KEY
-  
+export default function Sidebar({id}) {
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+  const [modalOpen, setModalOpen] = useState(false);
+  const conversationsOpen = activeKey === CONVERSATIONS_KEY;
+
   function closeModal() {
-    setModalOpen(false)
+    setModalOpen(false);
   }
 
   return (
-    <div style={{ width: '250px' }} className="d-flex flex-column">
+    <div style={{width: "250px"}} className="d-flex flex-column">
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
         <Nav variant="tabs" className="justify-content-center">
           <Nav.Item>
@@ -39,17 +39,22 @@ export default function Sidebar({ id }) {
         <div className="p-2 border-top border-right small">
           Your Id: <span className="text-muted">{id}</span>
         </div>
-        <Button onClick={() => setModalOpen(true)} className="rounded-0">
-          New {conversationsOpen ? 'Conversation' : 'Contact'}
+        <Button
+          style={{backgroundColor: "#"}}
+          onClick={() => setModalOpen(true)}
+          className="rounded-0"
+        >
+          New {conversationsOpen ? "Conversation" : "Contact"}
         </Button>
       </Tab.Container>
 
       <Modal show={modalOpen} onHide={closeModal}>
-        {conversationsOpen ?
-          <NewConversationModal closeModal={closeModal} /> :
+        {conversationsOpen ? (
+          <NewConversationModal closeModal={closeModal} />
+        ) : (
           <NewContactModal closeModal={closeModal} />
-        }
+        )}
       </Modal>
     </div>
-  )
+  );
 }
